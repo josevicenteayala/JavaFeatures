7da33c235737b27005162069348860ab7e620da2
Jose Vicente Ayala Luna <7784390+josevicenteayala@users.noreply.github.com>
2025-10-18 01:22:49 -0500

    upgrade gradle and junit
    Upgrade gradle and junit versions to support Java 25

--- Java17/build.gradle.kts
+++ Java17/build.gradle.kts
@@ -10,8 +10,9 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:6.0.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:6.0.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher:6.0.0")
 }
 
 tasks.test {
